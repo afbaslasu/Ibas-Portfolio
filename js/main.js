687923472e83b92352724e8c5e1677d7a6cc02a0
afbaslasu <afbaslasu@gmail.com>
2025-05-08 05:17:52 +0200

    chore: layout modification of project section

--- js/main.js
+++ js/main.js
@@ -12,18 +12,58 @@
 //   });
 // });
 
+// $(document).ready(function () {
+//   let $btns = $(".project-area .button-group button");
+//   let $grid = $(".grid").isotope({
+//     itemSelector: ".element-item",
+//     layoutMode: "fitRows",
+//   });
+
+//   $btns.click(function (e) {
+//     $btns.removeClass("active");
+//     $(this).addClass("active");
+
+//     let selector = $(this).attr("data-filter");
+//     $grid.isotope({ filter: selector });
+//   });
+// });
+// $(document).ready(function () {
+//   $(".filter-btn").click(function () {
+//     var filterValue = $(this).attr("data-filter");
+
+//     // Toggle active class
+//     $(".filter-btn").removeClass("active");
+//     $(this).addClass("active");
+
+//     if (filterValue === "*") {
+//       $(".element-item").show();
+//     } else {
+//       $(".element-item").hide();
+//       $(filterValue).show();
+//     }
+//   });
+
+//   $(".project-area .button-group #btn1").trigger("click");
+// });
 $(document).ready(function () {
-  let $btns = $(".project-area .button-group button");
-  let $grid = $(".grid").isotope({
+  // Initialize Isotope
+  var $grid = $(".grid").isotope({
     itemSelector: ".element-item",
     layoutMode: "fitRows",
   });
 
-  $btns.click(function (e) {
-    $btns.removeClass("active");
+  // Filter items on button click
+  $(".filter-btn").click(function () {
+    var filterValue = $(this).attr("data-filter");
+
+    // Toggle active class
+    $(".filter-btn").removeClass("active");
     $(this).addClass("active");
 
-    let selector = $(this).attr("data-filter");
-    $grid.isotope({ filter: selector });
+    // Apply filter
+    $grid.isotope({ filter: filterValue });
   });
+
+  // Trigger the 'All' button click on page load to initialize layout
+  $('.filter-btn[data-filter="*"]').trigger("click");
 });
