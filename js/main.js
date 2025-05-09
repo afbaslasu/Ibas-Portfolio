849e3ff2a71cdff51b9ecd82737357d76278c225
afbaslasu <afbaslasu@gmail.com>
2025-05-09 02:13:17 +0200

    chore: addition of footer to the index.html file and scroll effect function to the main.js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+  // Smooth scroll for nav links
+  $(".navbar-nav a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 70, // Adjust for fixed navbar height
+        },
+        800 // Scroll duration in milliseconds
+      );
+    }
+  });
+});
+
+$(document).ready(function () {
   // Initialize Isotope
   var $grid = $(".grid").isotope({
     itemSelector: ".element-item",
